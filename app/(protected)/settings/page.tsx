3df2c595cd330b953e8539bb510d597a35ab6eb8
Ekateryna Kravchuk <ekateryna.kravchuk@gmail.com>
2024-05-14 12:08:35 +0300

    credentials login functionality

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,10 +1,17 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 
 const SettingsPage = async () => {
   const seccion = await auth()
   return ( 
     <div>
       {JSON.stringify(seccion)}
+      <form action={async()=>{
+        'use server'
+        await signOut()
+      }}>
+        <Button>Sign Out</Button>
+      </form>
     </div>
     );
 }
